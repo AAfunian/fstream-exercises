a81b089fee5f8b0360fa74fae79120e6d108a372
Amir Afunian <amirafunian@outlook.com>
2021-10-13 13:29:57 -0700

    Implemented solution to exercise 3, which writes data from raw_temps.txt and outputs the mean and median temp data.

--- fstream exercises.cpp
+++ fstream exercises.cpp
@@ -29,10 +29,11 @@ void fill_reading(vector<Reading>& r, int n) {
 	}
 }
 ostream& operator<<(ostream& os, Reading& r) {
-	return os << "Temperature at " << r.hour << "h: " << r.temp << '\n';
+	return os << r.hour << " h: " << r.temp << '\n';
 }
 
 int main() {
+	// Writing data to raw_temps.txt
 	vector<Reading> readings;
 	int counter = 50;
 	ofstream ost{ "raw_temps.txt" };
@@ -40,6 +41,45 @@ int main() {
 	for (int i = 0; i < counter; ++i) {
 		ost << readings[i];
 	}
-
+	ost.close();
+	// Reading data from raw_temps.txt
+	vector<Reading> data;
+	ifstream ist{ "raw_temps.txt" };
+	ist.exceptions(ist.exceptions() | ios_base::badbit);
+	int h;
+	double t;
+	char temp, temp2;
+	while (true) {
+		if (ist >> h) {
+			ist >> temp >> temp2;
+			if (ist >> t && temp == 'h' && temp2 == ':') {
+				data.push_back(Reading(h, t));
+			}
+			else {
+				cout << "Error! (1)\n";
+				ist.unget();
+				ist.clear(ios_base::failbit);
+				return -1;
+			}
+		}
+		else if (ist.eof()) break;
+		else {
+			cout << "Error! (2)\n";
+			ist.unget();
+			ist.clear(ios_base::failbit);
+			return -1;
+		}
+	}
+	int sum = 0;
+	int median = 0;
+	for (int i = 0; i < data.size(); ++i) {
+		sum += data[i].temp;
+	}
+	if (counter % 2 == 0) {
+		median = (data[counter / 2].temp + data[(counter / 2) - 1].temp) / 2;
+	}
+	else median = data[counter / 2].temp;
+	// Output mean and median data
+	cout << "Mean: " << sum/data.size() << " Median: " << median << '\n';
 	return 0;
 }
